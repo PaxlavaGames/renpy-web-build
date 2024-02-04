e088912ab671a5335cf2dfaae4fd443021052ea0
quillcraftsman <quill@craftsman.lol>
2024-02-04 16:52:07 +0300

    add custom build command

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update
 RUN apt-get install --yes libxext6 mesa-utils
 
 COPY entrypoint.sh /entrypoint.sh
+COPY custom_web_build_command.rpy /custom_web_build_command.rpy
 
 RUN chmod +x ./entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Copying custom web command"
+./custom_web_build_command.rpy >> ./renpy/launcher/distribute.rpy
 echo "Trying to build project in '$1'..."
-./renpy/renpy.sh ./renpy/launcher distribute $1
+./renpy/renpy.sh ./renpy/launcher custom_web_build_command $1
 echo "Finished"
