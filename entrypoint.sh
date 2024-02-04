8dcfaa53fa70cf4d5908175f80bf975a1c1c5ec3
quillcraftsman <quill@craftsman.lol>
2024-02-04 17:11:32 +0300

    fix distribute file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 echo "Copying custom web command"
 ls
-ls ./renpy/launcher/
-chmod +rwx ./renpy/launcher/distribute.rpy
-./custom_web_build_command.rpy >> ./renpy/launcher/distribute.rpy
+ls ./renpy/launcher/game/
+chmod +rwx ./renpy/launcher/game/distribute.rpy
+./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy
+echo "Done"
 echo "Trying to build project in '$1'..."
 ./renpy/renpy.sh ./renpy/launcher custom_web_build_command $1
 echo "Finished"
