39cc817e0e00a4f77ac774d95f5e43044e023cfb
quillcraftsman <quill@craftsman.lol>
2024-02-04 17:07:50 +0300

    add rights and check dirs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 echo "Copying custom web command"
+ls
+ls ./renpy/launcher/
+chmod +rwx ./renpy/launcher/distribute.rpy
 ./custom_web_build_command.rpy >> ./renpy/launcher/distribute.rpy
 echo "Trying to build project in '$1'..."
 ./renpy/renpy.sh ./renpy/launcher custom_web_build_command $1
