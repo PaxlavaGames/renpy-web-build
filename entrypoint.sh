93180f873eecac9d6fb044c33257cf7c7339ffd6
quillcraftsman <quill@craftsman.lol>
2024-02-04 17:19:45 +0300

    trying bash -c option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Copying custom web command"
 ls
 ls ./renpy/launcher/game/
 chmod +rwx ./renpy/launcher/game/distribute.rpy
-./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy
+bash -c "./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy"
 echo "Done"
 echo "Trying to build project in '$1'..."
 ./renpy/renpy.sh ./renpy/launcher custom_web_build_command $1
