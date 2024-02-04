f6adf2cb831f942e12e221515e5a3f1ff8c4a3dd
quillcraftsman <quill@craftsman.lol>
2024-02-04 18:11:30 +0300

    trying delete web.rpy

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ ls
 ls ./renpy/launcher/game/
 chmod +rwx ./renpy/launcher/game/distribute.rpy
 cp custom_web_build_command.rpy ./renpy/launcher/game/custom_web_build_command.rpy
-bash -c "./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy"
+rm -rf ./renpy/launcher/game/web.rpy
+rm -rf ./renpy/launcher/game/web.rpyc
+# bash -c "./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy"
 echo "Done"
 echo "Trying to build project in '$1'..."
 ./renpy/renpy.sh ./renpy/launcher custom_web_build_command $1
