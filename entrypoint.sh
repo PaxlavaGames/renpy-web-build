2a88cb95de9a73d915a21b04533e1ce7be4cb374
quillcraftsman <quill@craftsman.lol>
2024-02-06 14:50:38 +0300

    use renpy web.py file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-echo "Copying custom web command"
-ls
-ls ./renpy/launcher/game/
-# chmod +rwx ./renpy/launcher/game/distribute.rpy
-# cp custom_web_build_command.rpy ./renpy/launcher/game/custom_web_build_command.rpy
-rm -rf ./renpy/launcher/game/web.rpy
-rm -rf ./renpy/launcher/game/web.rpyc
-cp web.rpy ./renpy/launcher/game/web.rpy
-# bash -c "./custom_web_build_command.rpy >> ./renpy/launcher/game/distribute.rpy"
-echo "Done"
+# echo "Copying custom web command"
+# ls
+# ls ./renpy/launcher/game/
+
+# rm -rf ./renpy/launcher/game/web.rpy
+# rm -rf ./renpy/launcher/game/web.rpyc
+# cp web.rpy ./renpy/launcher/game/web.rpy
+
+# echo "Done"
 echo "Trying to build project in '$1'..."
 ./renpy/renpy.sh ./renpy/launcher web_build $1
 echo "Finished"
