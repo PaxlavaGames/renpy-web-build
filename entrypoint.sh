a86be249ce62d2fced2516568a162b19adbb4d91
quillcraftsman <quill@craftsman.lol>
2024-02-07 16:32:48 +0300

    add build destination

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# echo "Copying custom web command"
-# ls
-# ls ./renpy/launcher/game/
-
-# rm -rf ./renpy/launcher/game/web.rpy
-# rm -rf ./renpy/launcher/game/web.rpyc
-# cp web.rpy ./renpy/launcher/game/web.rpy
-
 # echo "Done"
 echo "Trying to build project in '$1'..."
-./renpy/renpy.sh ./renpy/launcher web_build $1
+./renpy/renpy.sh ./renpy/launcher web_build $1 --destination ./web_build
 echo "Finished"
+echo "You can find your build in ./web_build folder"
